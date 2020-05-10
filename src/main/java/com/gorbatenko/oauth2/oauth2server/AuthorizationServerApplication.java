ce0291f0ab79424ba71052aca2b5532542712a68
Vladimir Gorbatenko <mail.gva@gmail.com>
2020-05-10 21:08:01 +0300

    Created oauth2-server

---
+++ src/main/java/com/gorbatenko/oauth2/oauth2server/AuthorizationServerApplication.java
@@ -0,0 +1,15 @@
+package com.gorbatenko.oauth2.oauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
